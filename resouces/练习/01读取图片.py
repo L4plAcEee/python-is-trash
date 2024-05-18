7e412b19f1c680be60310df2e6b1b9a928572fbd
L4plAcEee <1906518568@qq.com>
2024-05-18 08:54:03 +0800

    Delete resouces directory

--- resouces/练习/01读取图片.py
+++
@@ -1,7 +0,0 @@
-import cv2 as cv
-
-
-img = cv.imread("../pictures/AI.jpeg")
-cv.imshow("AI.jpeg", img)
-cv.waitKey(0)
-cv.destroyWindow("AI.jpeg")
--- resouces/练习/02图片灰度转换.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-
-img = cv.imread("../pictures/AI.jpeg")
-cv.imshow("AI_img", img)
-gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-cv.imshow("AI_img_gray", gray_img)
-cv.imwrite("../pictures/gray_AI_img.jpeg", gray_img)
-cv.waitKey()
-cv.destroyAllWindows()
